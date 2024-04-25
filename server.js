82b044e67347d60c6d74aa260c141a83a107a424
bootupdesigner <bootup.itinfo@gmail.com>
2024-04-24 20:40:53 -0400

    added more information to customer and owner email

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.post("/send", async function (req, res) {
             .filter(service => service.selected)
             .map(service => service.name);
 
+        const selectedContact = req.body.mailerState.contacts
+            .filter(contact => contact.selected)
+            .map(contact => contact.name);
+
         const firstMailOptions = {
             from: req.body.mailerState.email,
             to: process.env.EMAIL,
@@ -51,12 +55,20 @@ app.post("/send", async function (req, res) {
             html: `
             <div>
             <p>You've received a message from ${req.body.mailerState.name}</p>
-            <p>Message from customer: ${req.body.mailerState.message}</p>
+            <p>Message from customer:</p>
+            <ul>
+                <li>${req.body.mailerState.message}</li>
+            </ul>
             <p>Reason for inquiry:</p>       
             <ul>
-            ${selectedServices.map(service => `<li>${service}</li>`).join('')}
-
+                ${selectedServices.map(service => `<li>${service}</li>`).join('')}
             </ul>
+            <p>Preferred point of contact:</p>
+            <ul>
+            ${selectedContact.map(contact => `<li>${contact}</li>`).join('')}
+            </ul>
+            <p>Phone: ${req.body.mailerState.phone}</p>
+            <p>Email:${req.body.mailerState.email}</p>
         </div>`
         };
 
@@ -67,18 +79,40 @@ app.post("/send", async function (req, res) {
             to: req.body.mailerState.email,
             subject: `Curtis Shepard's Portfolio Response`,
             text: `Thanks for reaching us ${req.body.mailerState.name}. Curtis will be contacting you shortly`,
-            html:`
+            html: `
             <div>
-            <h1>Curtis Shepard | Web Developer</h1>
-            <p>Hi ${req.body.mailerState.name},</p>
-            <p>Thank you for considering me for your open position:</p>
-            <ul>
-            ${selectedServices.map(service => `<li>${service}</li>`).join('')}
-            </ul>
-            <p>I an be reached by phone Monday - Friday from 8:00am to 2:00pm and from 4:00pm 8:00pm</p>
-            <br/>
-            <p>Thanks for reading,</p>
-            <span>Curtis Shepard</span>
+                <h1 style="text-align:center;">Curtis Shepard | Web Developer</h1>
+
+                <p style="font-weight:bold;font-size:18px;">Hi ${req.body.mailerState.name},</p>
+
+                <p style="font-size:18px;">Thank you for considering me for your open position:</p>
+
+                <ul>
+                    ${selectedServices.map(service => `<li>${service}</li>`).join('')}
+
+                </ul>
+
+                <p style="font-size:18px;">If you've left instructions to respond, I will contact you as sson as possible. I can be reached by <a href=tel:+19543489783>phone</a> or <a href=mailto:shepardcurtis2@gmail.com>email</a>, Monday - Friday from 8:00am to 2:00pm and from 4:00pm 8:00pm.</p>
+
+                <br/>
+
+                <p>Thanks for reading,</p>
+
+                <br/>
+
+                <p style="font-size:18px;"><i>Curtis Shepard</i></p>
+
+                <br/>
+
+                <hr/>
+
+                <p style="font-size:18px;"><strong>Curtis Shepard</strong></p>
+
+                <p style="font-size:18px;"><a href=mailto:shepardcurtis2@gmail.com>shepardcurtis2@gmail.com</a></p>
+
+                <p style="font-size:18px;"><a href=tel:+19543489783>(954)348-9783</a></p>
+
+                <p style="font-size:18px;"><a href=https://bootupwebdesigns.com>bootupwebdesigns.com</a></p>
             </div>`
 
         };
